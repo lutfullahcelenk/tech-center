a7558e0c8f32a2e5bd7a1fa39d5d792a32c9b08c
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-07-17 11:28:23 +0300

    components and pages are created and routed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+//pages
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import AddItem from "./pages/AddItem";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="detail/:itemId" element={<Detail />} />
+      <Route path="add-item" element={<AddItem />}></Route>
+    </Routes>
   );
-}
+};
 
 export default App;
