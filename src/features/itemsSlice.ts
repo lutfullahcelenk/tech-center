7c21b0053f6fb42fa9f2dc541d4d5d60e5773d89
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-07-18 02:29:02 +0300

    detail page is completed

--- src/features/itemsSlice.ts
+++ src/features/itemsSlice.ts
@@ -2,12 +2,14 @@ import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 type ItemsState = {
   items: [];
+  selectedItem: any;
   loading: boolean;
   error: string;
 }
 
 const initialState: ItemsState = {
   items: [],
+  selectedItem: {},
   loading: false,
   error: "",
 };
@@ -18,15 +20,17 @@ export const fetchItems = createAsyncThunk("devices/fetchItems", async () => {
   ).then((res) => res.json());
 });
 
+export const getItem = createAsyncThunk("devices/getItem", async (id: any) => {
+  return fetch(
+    `https://62286b649fd6174ca82321f1.mockapi.io/case-study/products/${id}`
+  ).then((res) => res.json());
+});
+
 
 export const itemsSlice = createSlice({
   name: "devices",
   initialState,
-  reducers: {
-    getItem: (state, action: PayloadAction<any>) => {
-      return state.items.find((item:any)=> item.id === action.payload)
-    },
-  },
+  reducers: {},
   extraReducers: (builder: any) => {
     builder.addCase(fetchItems.pending, (state: ItemsState) => {
       state.loading = true;
@@ -41,10 +45,23 @@ export const itemsSlice = createSlice({
       state.items = [];
       state.error = "Yüklenemedi"
     })
+
+    //getItem
+    builder.addCase(getItem.pending, (state: ItemsState) => {
+      state.loading = true;
+    })
+    builder.addCase(getItem.fulfilled, (state: ItemsState, action:PayloadAction<[]>) => {
+      state.loading = false;
+      state.error = "";
+      state.selectedItem = action.payload
+    })
+    builder.addCase(getItem.rejected, (state: ItemsState) => {
+      state.loading = false;
+      state.selectedItem = [];
+      state.error = "Yüklenemedi"
+    })
   }
 });
 
-
-export const { getItem } = itemsSlice.actions
 
 export default itemsSlice.reducer;
--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -7,16 +7,35 @@ import { useAppDispatch, useAppSelector } from "../../store/hooks";
 const Detail = () => {
   const dispatch = useAppDispatch();
   const { itemId } = useParams();
+  const { selectedItem } = useAppSelector((state) => state.items);
 
   useEffect(() => {
     dispatch(getItem(itemId));
   }, [itemId]);
 
   return (
-    <div className="flex flex-col xl:px-20 max-w-6xl mx-auto mt-12">
-      <div className="flex">
-        <img src={""} alt="device" />
-        <div>as</div>
+    <div className="flex flex-col xl:px-20 max-w-6xl mx-auto mt-12 divide-y divide-black">
+      <div className="flex flex-col sm:flex-row gap-x-8 pb-10">
+        <img
+          className="bg-white w-60 p-8 rounded shadow-lg"
+          src={selectedItem.avatar}
+          alt="device"
+        />
+
+        <div className="flex flex-col justify-between items-start pt-4 sm:pt-0">
+          <div>
+            <h2 className="text-3xl tracking-wider font-semibold lowercase first-letter:uppercase">
+              {selectedItem.name}
+            </h2>
+            <p className="text-left text-gray-500 italic">{selectedItem.category}</p>
+          </div>
+          <p className="text-base">$ {selectedItem.price}</p>
+        </div>
+      </div>
+
+      <div className="text-left">
+        <h4 className="font-semibold tracking-wide py-4">Description</h4>
+        <p>{selectedItem.description}</p>
       </div>
     </div>
   );
